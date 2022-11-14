1f84d2ae2ca11ca7159c36fac6f57bc3e0934247
ihsunmozdeh <ihsun@tuta.io>
2022-11-15 02:40:25 +0330

    🚀 Add movie models.

---
+++ movie/models.py
@@ -0,0 +1,6 @@
+from movie.submodels.comment import Comment
+from movie.submodels.crew import Crew
+from movie.submodels.genre import Genre
+from movie.submodels.movie import Movie
+from movie.submodels.movie_crew import MovieCrew
+from movie.submodels.role import MovieRole
