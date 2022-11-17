0a6115ffe24b55352b432a68b2b2e086e22c318b
ihsunmozdeh <ihsun@tuta.io>
2022-11-17 04:53:15 +0330

    🚀 Add debug-task. (Celery)

--- accounts/tasks.py
+++ accounts/tasks.py
@@ -1,12 +1,23 @@
+from time import sleep
 from celery.decorators import task
 from celery.utils.log import get_task_logger
 from django.core.mail import EmailMessage
-from time import sleep
 
 logger = get_task_logger(__name__)
 
+
+@task(name='debug_task', bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
+
+
 @task(name='send_email_task')
 def send_mail_task(subject, message, email_from, recipient_list):
-    message = EmailMessage(subject, message, email_from, recipient_list)
+    message = EmailMessage(subject,
+        message,
+        email_from,
+        recipient_list
+    )
+    
     message.content_subtype = 'html'
     message.send()
