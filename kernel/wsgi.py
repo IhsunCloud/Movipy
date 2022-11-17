3d8f55eaa6c0fd1239edc844e58b2e2c54132b30
ihsunmozdeh <ihsun@tuta.io>
2022-11-17 04:54:33 +0330

    🚀 Add Celery stuff.

--- kernel/wsgi.py
+++ kernel/wsgi.py
@@ -14,3 +14,27 @@ from django.core.wsgi import get_wsgi_application
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kernel.settings')
 
 application = get_wsgi_application()
+
+
+"""
+Celery stuff.
+"""
+# from __future__ import absolute_import
+# import os
+# from celery import Celery
+# from django.conf import settings
+
+"""
+Default settings module for the 'celery' program.
+"""
+# os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'kernel.settings.development')
+# app = Celery('kernel')
+
+"""
+Using a string here means the worker will not have to
+pickle the object when using Windows.
+"""
+# app.config_from_object('django.conf:settings')
+# app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+
+
