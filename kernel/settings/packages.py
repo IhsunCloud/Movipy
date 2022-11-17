8c50e49b0c7035d132c15f2f4748f1e56ad86874
ihsunmozdeh <ihsun@tuta.io>
2022-11-17 05:08:33 +0330

    🚀 Add Celery stuff.

--- kernel/settings/packages.py
+++ kernel/settings/packages.py
@@ -67,10 +67,15 @@ ROBOTS_SITEMAP_VIEW_NAME = 'sitemap'
 # HTML Minfier Configs -->
 HTML_MINIFY = True
 
+
+"""
 # CELERY STUFF
+
 BROKER_URL = 'redis://localhost:6379'
 CELERY_RESULT_BACKEND = 'redis://localhost:6379'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TIMEZONE = 'Asia/Tehran'
+
+"""
