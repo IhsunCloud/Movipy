b5b8302c3c6d5b7b69f9e4e69a8e0e47f7ad1317
ihsunmozdeh <ihsun@tuta.io>
2022-11-17 08:40:42 +0330

    🚀 Change admin's title.

--- movie/admin.py
+++ movie/admin.py
@@ -3,6 +3,6 @@ from django.utils.translation import gettext_lazy as _
 
 from movie.subadmins.movie_admin import MovieAdmin
 
-admin.site.site_header = _("Moviepy Admin")
-admin.site.site_title = _("Moviepy Admin Portal")
-admin.site.index_title = _("Welcome to Moviepy Portal")
+admin.site.site_header = _("Movipy Admin")
+admin.site.site_title = _("Movipy Admin Portal")
+admin.site.index_title = _("Welcome to Movipy Portal")
