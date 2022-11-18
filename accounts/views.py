52bfd79558dfb5d2d3518686dc3917ef6f8ae72d
ihsunmozdeh <ihsun@tuta.io>
2022-11-18 23:55:17 +0330

    🚀 Changed signup view to generic views.

--- accounts/views.py
+++ accounts/views.py
@@ -1,18 +1,14 @@
 from django.contrib.auth import login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
+from django.views import generic
+from django.urls import reverse_lazy
 
 
-def signup_view(request):
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('/')
-    else:
-        form = UserCreationForm()
-    return render(request, '/', {'form': form})
+class SignUpView(generic.CreateView):
+    template_name = 'pages/sign-up.html'
+    form_class = UserCreationForm
+    success_url = reverse_lazy("signup")
 
 
 def login_view(request):
