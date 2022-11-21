3a398294f3c9b6945863c88e4438afd0fd17bbcf
ihsunmozdeh <ihsun@tuta.io>
2022-11-22 02:26:17 +0330

    🚀 Add sign in url.

--- accounts/views.py
+++ accounts/views.py
@@ -1,4 +1,4 @@
-from django.contrib.auth import login, logout
+from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.shortcuts import render, redirect
 from django.views import generic
@@ -9,6 +9,20 @@ class SignUpView(generic.CreateView):
     template_name = 'pages/sign-up.html'
     form_class = UserCreationForm
     success_url = reverse_lazy("signup")
+    
+    
+class LoginView(generic.View):
+    
+    def post(self, request):
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(username=username, password=password)
+
+        if user is not None:
+              login(request, user)
+              return redirect('/')
+        else:
+            return("please sign up")
 
 
 def login_view(request):
